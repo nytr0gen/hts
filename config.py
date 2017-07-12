554fac3a5ecd0fd3b49eeccef37023056cf9ee3e
Robert Vulpe <nytr0gen.george@gmail.com>
2017-07-12 22:08:35 +0300

    config.json changes, added mongo indexes

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import os
 import json
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING, TEXT
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-with open('config.json') as f:
+config_filepath = os.path.join(BASE_DIR, 'config.json')
+with open(config_filepath) as f:
     config = json.load(f)
 
-client = MongoClient(config['mongo_url'])
-db = client[config['mongo_db']]
+client = MongoClient(config['mongo']['url'])
+db = client[config['mongo']['db']]
+
+db.items.create_index([('created', DESCENDING)])
+db.items.create_index('subreddit')
+db.items.create_index([('text', TEXT)], background=True) # sparse text
